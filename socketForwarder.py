c076a6f92dc02f3dcb2738336c23a8318b4efdaa
clazzor <clazzor@slapnet.cz>
2025-02-17 18:01:29 +0100

    FIx for newer python

--- socketForwarder.py
+++ socketForwarder.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-import sys, socket, time, logging
+import sys
+import time
+import socket
+import logging
 from threading import Thread
+from typing import List
 
-logging.getLogger('').setLevel(logging.DEBUG) 
+logging.getLogger('').setLevel(logging.DEBUG)
 
 class HostConvertor:
     def __init__(self, host: str) -> None:
@@ -11,7 +15,7 @@ class HostConvertor:
         self.port = self.address = None
 
         self.hostSeperate()
-        if self.isValid: 
+        if self.isValid:
             self.isValidAddress()
             self.isValidPort()
 
@@ -65,21 +69,21 @@ class Sessioner:
         self.isConnect = False
         self.socket.close()
         self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        
+
         logging.warning(f' {self.role} target {self.textHost} disconnect, trying reconnecting.')
         while not self.isConnect:
             self._connect()
             time.sleep(1.5)
         logging.info(f' {self.role} target {self.textHost} is back online!')
-        
+
     def connect(self) -> None:
         self._connect()
         if self.isConnect:
             logging.info(f' {self.role} target {self.textHost} is connected.')
         else:
             logging.error(f' Could connect to {self.role.lower()} {self.textHost}')
-            self.reconnect() 
-    
+            self.reconnect()
+
     def reconnect(self) -> None:
         if(self.role == "Source"):
             self._reconnect()
@@ -102,11 +106,11 @@ class Sessioner:
         except KeyboardInterrupt:
             self._close()
         except:
-            self.reconnect()        
+            self.reconnect()
 
 
 class DataForwarder:
-    def __init__(self, hosts: list[str]) -> None:
+    def __init__(self, hosts: List[str]) -> None:
         isWithourErr = True
         self.hosts = []
 
@@ -115,7 +119,9 @@ class DataForwarder:
             if not _host.isValid:
                 isWithourErr = False
                 continue
-            self.hosts.append(Sessioner("Source" if i == 0 else "Destination", _host.address, _host.port))
+            self.hosts.append(Sessioner("Source" if i == 0 else "Destination",
+                                        _host.address,
+                                          _host.port))
         if not isWithourErr: sys.exit(1)
 
     def makeConnections(self) -> None:
@@ -129,11 +135,12 @@ class DataForwarder:
             for host in self.hosts[1:]:
                 if host.isConnect:
                     host.sendData(data)
-            
+
 
 def info():
     if len(sys.argv) < 3:
-        if len(sys.argv) > 1: logging.error(f' Not enough arguments!')
+        if len(sys.argv) > 1:
+            logging.error('Not enough arguments!')
         print(f'Usage: {sys.argv[0]} <source:port> <destination:port> [other destination . .]\n' +
                 '       Source & Destination must be IPv4 or domain, with define port.\n' +
                 '       Number of destination is not limited.')
